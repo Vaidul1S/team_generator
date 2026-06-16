ac9d17da9bb7f734b9ca4a454aebaee1198a8f43
Vaidul1S <vaidas.ad25@gmail.com>
2026-06-16 11:18:17 +0300

    06.16

--- app/(tabs)/bracket.jsx
+++ app/(tabs)/bracket.jsx
@@ -1,6 +1,7 @@
 import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Bracket() {
     document.title = "Team Bracket";
@@ -81,111 +82,117 @@ export default function Bracket() {
     };
 
     return (
-        <ScrollView>
-            <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <View style={styles.card}>
 
-                <View style={styles.col}>
-                    <View style={styles.row}>
-                        <TouchableOpacity onPress={_ => promoteTeam(bracket[0])} style={styles.button}>
-                            <Text style={styles.text}>(A1) {bracket[0]}</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.point}> : </Text>
-                        <TouchableOpacity onPress={_ => promoteTeam(bracket[7])} style={styles.button}>
-                            <Text style={styles.text}>(B4) {bracket[7]}</Text>
-                        </TouchableOpacity>
+                    <View style={styles.col}>
+                        <View style={styles.row}>
+                            <TouchableOpacity onPress={_ => promoteTeam(bracket[0])} style={styles.button}>
+                                <Text style={styles.text}>(A1) {bracket[0]}</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.point}> : </Text>
+                            <TouchableOpacity onPress={_ => promoteTeam(bracket[7])} style={styles.button}>
+                                <Text style={styles.text}>(B4) {bracket[7]}</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.row}>
+                            <TouchableOpacity onPress={_ => promoteTeam(bracket[5])} style={styles.button}>
+                                <Text style={styles.text}>(B2) {bracket[5]}</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.point}> : </Text>
+                            <TouchableOpacity onPress={_ => promoteTeam(bracket[2])} style={styles.button}>
+                                <Text style={styles.text}>(A3) {bracket[2]}</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.row}>
+                            <TouchableOpacity onPress={_ => promoteTeam(bracket[1])} style={styles.button}>
+                                <Text style={styles.text}>(A2) {bracket[1]}</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.point}> : </Text>
+                            <TouchableOpacity onPress={_ => promoteTeam(bracket[6])} style={styles.button}>
+                                <Text style={styles.text}>(B3) {bracket[6]}</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.row}>
+                            <TouchableOpacity onPress={_ => promoteTeam(bracket[4])} style={styles.button}>
+                                <Text style={styles.text}>(B1) {bracket[4]}</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.point}> : </Text>
+                            <TouchableOpacity onPress={_ => promoteTeam(bracket[3])} style={styles.button}>
+                                <Text style={styles.text}>(A4) {bracket[3]}</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <View style={styles.row}>
-                        <TouchableOpacity onPress={_ => promoteTeam(bracket[5])} style={styles.button}>
-                            <Text style={styles.text}>(B2) {bracket[5]}</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.point}> : </Text>
-                        <TouchableOpacity onPress={_ => promoteTeam(bracket[2])} style={styles.button}>
-                            <Text style={styles.text}>(A3) {bracket[2]}</Text>
-                        </TouchableOpacity>
+
+                    <View style={styles.col}>
+                        <View style={styles.row}>
+                            <TouchableOpacity onPress={_ => promoteToFinals(semifinals[0])} style={styles.button}>
+                                <Text style={styles.text}>(Q1) {semifinals[0]}</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.point}> : </Text>
+                            <TouchableOpacity onPress={_ => promoteToFinals(semifinals[1])} style={styles.button}>
+                                <Text style={styles.text}>(Q2) {semifinals[1]}</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.row}>
+                            <TouchableOpacity onPress={_ => promoteToFinals(semifinals[2])} style={styles.button}>
+                                <Text style={styles.text}>(Q3) {semifinals[2]}</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.point}> : </Text>
+                            <TouchableOpacity onPress={_ => promoteToFinals(semifinals[3])} style={styles.button}>
+                                <Text style={styles.text}>(Q4) {semifinals[3]}</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <View style={styles.row}>
-                        <TouchableOpacity onPress={_ => promoteTeam(bracket[1])} style={styles.button}>
-                            <Text style={styles.text}>(A2) {bracket[1]}</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.point}> : </Text>
-                        <TouchableOpacity onPress={_ => promoteTeam(bracket[6])} style={styles.button}>
-                            <Text style={styles.text}>(B3) {bracket[6]}</Text>
-                        </TouchableOpacity>
+
+                    <View style={styles.col}>
+                        <View style={styles.row}>
+                            <Text style={styles.title}>Finals🏆</Text>
+                        </View>
+                        <View style={styles.row}>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.text}>🥇(W1) {finals[0]}</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.point}> : </Text>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.text}>(W2) {finals[1]}</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <View style={styles.row}>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.text}>🥉(L1) {finals[2]}</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.point}> : </Text>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.text}>(L2) {finals[3]}</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <View style={styles.row}>
-                        <TouchableOpacity onPress={_ => promoteTeam(bracket[4])} style={styles.button}>
-                            <Text style={styles.text}>(B1) {bracket[4]}</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.point}> : </Text>
-                        <TouchableOpacity onPress={_ => promoteTeam(bracket[3])} style={styles.button}>
-                            <Text style={styles.text}>(A4) {bracket[3]}</Text>
-                        </TouchableOpacity>
-                    </View>
+
                 </View>
-
-                <View style={styles.col}>
-                    <View style={styles.row}>
-                        <TouchableOpacity onPress={_ => promoteToFinals(semifinals[0])} style={styles.button}>
-                            <Text style={styles.text}>(Q1) {semifinals[0]}</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.point}> : </Text>
-                        <TouchableOpacity onPress={_ => promoteToFinals(semifinals[1])} style={styles.button}>
-                            <Text style={styles.text}>(Q2) {semifinals[1]}</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.row}>
-                        <TouchableOpacity onPress={_ => promoteToFinals(semifinals[2])} style={styles.button}>
-                            <Text style={styles.text}>(Q3) {semifinals[2]}</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.point}> : </Text>
-                        <TouchableOpacity onPress={_ => promoteToFinals(semifinals[3])} style={styles.button}>
-                            <Text style={styles.text}>(Q4) {semifinals[3]}</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-
-                <View style={styles.col}>
-                    <View style={styles.row}>
-                        <Text style={styles.title}>Finals🏆</Text>
-                    </View>
-                    <View style={styles.row}>
-                        <TouchableOpacity style={styles.button}>
-                            <Text style={styles.text}>🥇(W1) {finals[0]}</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.point}> : </Text>
-                        <TouchableOpacity style={styles.button}>
-                            <Text style={styles.text}>(W2) {finals[1]}</Text>
-                        </TouchableOpacity>
-                    </View>
-
-                    <View style={styles.row}>
-                        <TouchableOpacity style={styles.button}>
-                            <Text style={styles.text}>🥉(L1) {finals[2]}</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.point}> : </Text>
-                        <TouchableOpacity style={styles.button}>
-                            <Text style={styles.text}>(L2) {finals[3]}</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+
+        backgroundColor: '#40b0cc80',
+    },
+    card: {
         flexDirection: 'column',
         alignContent: 'space-around',
         flexWrap: 'wrap',
         width: '100%',
-        backgroundColor: '#40b0cc80',
     },
     col: {
         justifyContent: 'space-around',
         height: '100%',
-        padding: '2%',
+        padding: '1%',
         width: '33%',
         backgroundColor: 'none',
     },
