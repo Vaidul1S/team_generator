17268bed35e94beb61c8c09b55c613a0c03eef49
Vaidul1S <vaidas.ad25@gmail.com>
2026-06-17 12:12:53 +0300

    06.17

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,9 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin', 
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      'react-native-reanimated/plugin',
     ],
   };
 };
