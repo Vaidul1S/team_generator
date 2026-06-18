94a1d4067f4bee6c6d991b64c4b4e6c01e97f14a
Vaidul1S <vaidas.ad25@gmail.com>
2026-06-18 13:36:07 +0300

    06.18

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,6 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
