43cb97a13346d99d0a7581a8b4ff3a8b8ca18f55
Vaidul1S <vaidas.ad25@gmail.com>
2026-07-01 10:47:41 +0300

    07.01

--- app/(tabs)/credits.jsx
+++ app/(tabs)/credits.jsx
@@ -9,11 +9,11 @@ export default function Credits() {
       <Image
         source={require("../../assets/images/gg.jpg")}
         style={styles.image} />
-      <Text style={styles.text}>Simple team generator for pairing people into teams.</Text>
+      <Text style={styles.text}>Team generator for pairing people into teams.</Text>
       <Text style={styles.text}>To delete name press long on a selected name.</Text>
       <Text style={styles.text}>Version 1.06</Text>
       <Text style={styles.text}>Added Tornament Table</Text>
-      <Text style={styles.text}>Added Tornament Bracket</Text>
+      <Text style={styles.text}>Added Tornament Bracket (Single elimination)</Text>
       <Link style={styles.link} href='https://github.com/Vaidul1S' target='_blank'>GitHub Link</Link>
       <Text style={styles.text}>&copy; Vaidul1s {new Date().getFullYear()}</Text>
     </View>
