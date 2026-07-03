b55873c2655e5565bae4498b9b0bf9166b8bb081
Vaidul1S <vaidas.ad25@gmail.com>
2026-07-03 14:33:14 +0300

    07.03

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -7,22 +7,48 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      home: NavClass(),
+    );
+  }
+}
+
+class NavClass extends StatefulWidget {
+  const NavClass({super.key});
+
+  @override
+  State<NavClass> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<NavClass> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(title: Text('Team Generator'), centerTitle: true),
         bottomNavigationBar: NavigationBar(
           destinations: [
             NavigationDestination(icon: Icon(Icons.home), label: 'Generator'),
             NavigationDestination(
-              icon: Icon(Icons.credit_score),
+              icon: Icon(Icons.copyright_outlined),
               label: 'Credits',
             ),
+            NavigationDestination(icon: Icon(Icons.table_view), label: 'Table'),
+            NavigationDestination(
+              icon: Icon(Icons.mediation),
+              label: 'Bracket',
+            ),
           ],
+          onDestinationSelected: (int value) {
+            setState((){
+              currentIndex = value;
+            });
+          },
+          selectedIndex: currentIndex,
         ),
-      ),
-    );
+      );
   }
 }
