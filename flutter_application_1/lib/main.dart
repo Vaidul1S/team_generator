4232b73e7b2272e6ff511e94b76fc9c4231e7ca2
Vaidul1S <vaidas.ad25@gmail.com>
2026-07-03 15:24:39 +0300

    07.03

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/credits.dart';
 import 'generator.dart'; // if you split it into its own file
 
+List<Widget> pages = [
+  Generator(),
+  Credits()
+];
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: NavClass(),
-    );
+    return MaterialApp(home: NavClass());
   }
 }
 
@@ -20,35 +23,34 @@ class NavClass extends StatefulWidget {
   const NavClass({super.key});
 
   @override
-  State<NavClass> createState() => _MyWidgetState();
+  State<NavClass> createState() => _NavClassState();
 }
 
-class _MyWidgetState extends State<NavClass> {
+class _NavClassState extends State<NavClass> {
   int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Team Generator'), centerTitle: true),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: 'Generator'),
-            NavigationDestination(
-              icon: Icon(Icons.copyright_outlined),
-              label: 'Credits',
-            ),
-            NavigationDestination(icon: Icon(Icons.table_view), label: 'Table'),
-            NavigationDestination(
-              icon: Icon(Icons.mediation),
-              label: 'Bracket',
-            ),
-          ],
-          onDestinationSelected: (int value) {
-            setState((){
-              currentIndex = value;
-            });
-          },
-          selectedIndex: currentIndex,
-        ),
-      );
+      body: currentIndex == 0 ? Center(child: Generator()) : null,
+
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Colors.black,
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Generator'),
+          NavigationDestination(
+            icon: Icon(Icons.copyright_outlined),
+            label: 'Credits',
+          ),
+          NavigationDestination(icon: Icon(Icons.table_view), label: 'Table'),
+          NavigationDestination(icon: Icon(Icons.mediation), label: 'Bracket'),
+        ],
+        onDestinationSelected: (int value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        selectedIndex: currentIndex,
+      ),
+    );
   }
 }
