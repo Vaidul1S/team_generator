443542b1ad7e753f8d8c2c945f9b1ab48beeca67
Vaidul1S <vaidas.ad25@gmail.com>
2026-07-21 13:17:21 +0300

    07.21

--- app/(tabs)/credits.jsx
+++ app/(tabs)/credits.jsx
@@ -13,7 +13,7 @@ export default function Credits() {
       <Text style={styles.text}>To delete name press long on a selected name.</Text>
       <Text style={styles.text}>Version 1.06</Text>
       <Text style={styles.text}>Added Tornament Table</Text>
-      <Text style={styles.text}>Added Tornament Bracket (Single elimination)</Text>
+      <Text style={styles.text}>Added Tornament Bracket (Single elimination, 2 groups)</Text>
       <Link style={styles.link} href='https://github.com/Vaidul1S' target='_blank'>GitHub Link</Link>
       <Text style={styles.text}>&copy; Vaidul1s {new Date().getFullYear()}</Text>
     </View>
